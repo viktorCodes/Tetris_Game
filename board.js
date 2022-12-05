38e2155c9623cec4c0875ed56a94ae2ae5ecc16c
calimendoza <mbaivictor1@gmail.com>
2022-12-05 07:32:46 -0800

    Tetris Game starter code

---
+++ board.js
@@ -0,0 +1,14 @@
+class Board {
+  
+    // Reset the board when we start a new game.
+    reset() {
+      this.grid = this.getEmptyBoard();
+    }
+    
+    // Get matrix filled with zeros.
+    getEmptyBoard() {
+      return Array.from(
+        {length: ROWS}, () => Array(COLS).fill(0)
+      );
+    }
+  }
